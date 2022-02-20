3b33444faf3faceac1990fe1a6463c8fedbdda58
samankey <71953671+samankey@users.noreply.github.com>
2022-02-20 21:21:18 +0900

    Add: 네비게이션 메뉴 링크 이동

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
      :contents="'👀 다른 포스팅도 읽어보기!'"
     />
     <ListUser />
+    <NoItem />
     <ButtonDefault :text="'포스팅 하러가기'" :style-type="'main'" />
   </div>
 </template>
@@ -12,12 +13,14 @@
 <script>
 import ImageHeader from '@/components/default/default/imageHeader';
 import ListUser from '@/components/default/list/listUser';
+import NoItem from '@/components/default/default/noItem';
 import ButtonDefault from '@/components/default/button/buttonDefault';
 
 export default {
   components: {
     ImageHeader,
     ListUser,
+    NoItem,
     ButtonDefault
   },
 
